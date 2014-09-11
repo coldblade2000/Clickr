f51fe65029cd041b220a2dbc5c9ab91187977ddf
coldblade2000 <diegogranada7@gmail.com>
2014-09-11 16:38:37 -0500

    Very first  commit

--- app/src/main/java/com/twotowerstudios/clickr/MainMenu.java
+++ app/src/main/java/com/twotowerstudios/clickr/MainMenu.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainMenu extends Activity {
 
+    //Hello
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
