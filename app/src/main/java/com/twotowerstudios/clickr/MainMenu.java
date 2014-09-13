71ada92d3e28f00985fd94844b1b61543fce714b
coldblade2000 <diegogranada7@gmail.com>
2014-09-13 00:11:58 -0500

    Changed Main Menu to Linear Layout

--- app/src/main/java/com/twotowerstudios/clickr/MainMenu.java
+++ app/src/main/java/com/twotowerstudios/clickr/MainMenu.java
@@ -2,37 +2,25 @@ package com.twotowerstudios.clickr;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainMenu extends Activity {
 
     //Hello
+    TextView tvName, tvStudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        initialize();
+
     }
 
+    private void initialize() {
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvStudio = (TextView) findViewById(R.id.tvStudio);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
